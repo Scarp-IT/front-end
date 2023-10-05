e20f0993e52f8cbbb681c762a2d874db3227dfac
uedarap <uedarap@gmail.com>
2023-10-05 08:29:23 -0300

    Pagina inicial ✅

--- scarpIT-front-end/src/App.vue
+++ scarpIT-front-end/src/App.vue
@@ -1,9 +1,18 @@
 <template>
     <main>
-        <div class="home"></div>
-        <div class="servicos"></div>
-        <div class="scarpidia"></div>
-        <div class="contato"></div>
+        <div id="home">
+            <Menu></Menu>
+            <div id="frase">
+                <h1>Scarp.IT</h1>
+                <h1>Scarp.IT</h1>
+                <p id="sub">Seus Guadiões Digitais ;)</p>
+                <img src="./assets/easterEgg.svg" alt="">
+            </div>
+        </div>
+        <img id="bg" src="./assets/BGsvg.svg" alt="">
+        <div id="servicos"></div>
+        <div id="scarpidia"></div>
+        <div id="contato"></div>
     </main>
 </template>
 
@@ -12,6 +21,18 @@
 </script>
 
 <style lang="scss" scoped>
+
+    @font-face {
+        font-family: 'Jet Brains';
+        src: url('./assets/fonts/JetBrainsMono-Regular.ttf') format('truetype');
+        font-weight: normal;
+    }
+
+    @font-face {
+        font-family: 'Jet Brains-Bold';
+        src: url('./assets/fonts/JetBrainsMono-Bold.ttf') format('truetype');
+        font-weight: bold;
+    }
 
     main{
         width: 100vw;
@@ -27,5 +48,57 @@
     main > div:first-child{
         // background-color: rgba(255, 255, 255, 0.143);
     }
+
+    #home{
+        padding-top: 35px;
+        display: grid;
+        justify-content: center;
+    }
+
+    #frase{
+        font-family: 'Jet Brains-Bold';
+        font-weight: bold;
+        z-index: 10;
+
+        h1:first-child{
+            top: 40.5%;
+            left: 50.5%;
+            color: #8365F4;
+        }
+        
+        *{
+            position: absolute;
+            font-size: 140px;
+            top: 40%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            color: #fff;
+        }
+
+        p{
+            font-family: 'Jet Brains';
+            font-size: 36px;
+            top: 62%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+
+        img{
+            left: 82%;
+            top: 60%;
+            width: 6rem;
+        }
+    }
+
+    #bg{
+        position: absolute;
+        width: 100vw;
+        top: 70%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        filter: blur(0.5px);
+        z-index: 9;
+    }
+    
 
 </style>
--- scarpIT-front-end/src/components/Menu.vue
+++ scarpIT-front-end/src/components/Menu.vue
@@ -1,8 +1,34 @@
-<template></template>
+<template>
+    <div id="menu">
+        <h3 style="font-family: 'Jet Brains';">Scarp.IT</h3>
+        <div id="btn-wiki" :onclick="clickBtnWiki">
+            <h2 style="font-family: 'Jet Brains'; margin: 0;">Wiki</h2>
+        </div>
+    </div>
+</template>
 
 <script setup lang="ts">
-    import { ref } from "vue";
+    function clickBtnWiki(){
+        window.open('https://www.scarpit.tech/wiki', '_blank');
+    }
 </script>
 
 
-<style scoped></style>
+<style scoped lang="scss">
+    #menu{
+        display: flex;
+        width: 70vw;
+        height: 60px;
+        border-bottom: 1px solid #ADA4CD;
+        justify-content: space-between;
+    }
+
+    #btn-wiki{
+        width: 155px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 1px solid #ADA4CD;
+        cursor: pointer;
+    }
+</style>
