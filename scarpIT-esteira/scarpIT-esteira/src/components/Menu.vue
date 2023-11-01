9de12c34fd276913a95cf4c3cd9f5be403cc61a8
uedarap <uedarap@gmail.com>
2023-11-01 12:49:02 -0300

    Iniciando Esteira ⚠️

---
+++ scarpIT-esteira/scarpIT-esteira/src/components/Menu.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="menu">
+        <h3 style="font-family: 'Jet Brains';">Scarp.IT</h3>
+        <div id="btn-wiki">
+            <h2 style="font-family: 'Jet Brains'; margin: 0;">Wiki</h2>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    // function clickBtnWiki(){
+    //     window.open('http://wiki.scarpit.tech', '_blank');
+    // }
+</script>
+
+
+<style lang="scss" scoped>
+    #menu{
+        display: flex;
+        width: 70vw;
+        height: 60px;
+        border-bottom: 1px solid #ADA4CD;
+        justify-content: space-between;
+        z-index: 10;
+    }
+
+    #btn-wiki{
+        width: 155px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 1px solid #ADA4CD;
+        cursor: pointer;
+    }
+</style>
